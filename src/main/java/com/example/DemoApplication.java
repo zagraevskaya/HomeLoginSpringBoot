9c909d8189a96894157f96f3b6963a14314edad0
zagraevskaya@gmail.com <Nata0908>
2018-08-29 12:59:51 +0300

    db_accord

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,12 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+       SpringApplication.run(DemoApplication.class, args);
+    
+    }
 }
